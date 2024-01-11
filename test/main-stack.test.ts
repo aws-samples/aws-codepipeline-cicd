5cf1f438e8d7490b4d93aa5351fa8e34d9b20852
Konstantin Zarudaev <zarudk@amazon.com>
2024-01-11 15:17:16 +0100

    Update tests. Implement simple logic for different behaviour. Ready to review and merge.

--- test/main-stack.test.ts
+++ test/main-stack.test.ts
@@ -2,16 +2,46 @@ import * as cdk from 'aws-cdk-lib'
 import { Template } from 'aws-cdk-lib/assertions'
 import { MainStack } from '../lib/main-stack'
 
-const app = new cdk.App()
-const stack = new MainStack(app, 'Dev-MainStack')
-const template = Template.fromStack(stack)
+describe('Unit tests for the main stack in DEV', () => {
+  const app = new cdk.App()
+  const stack = new MainStack(app, 'Dev-MainStack')
+  const template = Template.fromStack(stack)
 
-test('Event Bus has been created', () => {
-  // Assessment
-  template.hasResource('AWS::Events::EventBus', '')
+  test('Event Bus has been created', () => {
+    // Assessment
+    template.hasResource('AWS::Events::EventBus', '')
+  })
+
+  test('MainStack has output', () => {
+    // Assessment
+    template.hasOutput('EventBusName', '')
+  })
+
+  // Summary checks
+  test('Expected number of the event buses', () => {
+    const expectedValue = 2 // Two event busses are expected
+    template.resourceCountIs('AWS::Events::EventBus', expectedValue)
+  })
 })
 
-test('MainStack has output', () => {
-  // Assessment
-  template.hasOutput('EventBusName', '')
+describe('Unit tests for the main stack in PROD', () => {
+  const app = new cdk.App()
+  const stack = new MainStack(app, 'Prod-MainStack')
+  const template = Template.fromStack(stack)
+
+  test('Event Bus has been created', () => {
+    // Assessment
+    template.hasResource('AWS::Events::EventBus', '')
+  })
+
+  test('MainStack has output', () => {
+    // Assessment
+    template.hasOutput('EventBusName', '')
+  })
+
+  // Summary checks
+  test('Expected number of the event buses', () => {
+    const expectedValue = 1 // Only one event bus is expected
+    template.resourceCountIs('AWS::Events::EventBus', expectedValue)
+  })
 })
